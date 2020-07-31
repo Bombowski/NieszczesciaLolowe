548775b4fd471ed5275deec742b277f20aa0dc32
HolyCatPL <erwindarul@gmail.com>
2020-07-31 21:43:57 +0200

    Zrobione TestRegExChamp
    Format sie nie sprawdza w average kdacss i game time; w obu przypadkach zaokraglenie jest niepoprawne, a w time sekundy sa niepoprawnie wyswietlane jak sa jedno cyfrowe

--- src/nieszczescialolowe/test/TestRegExChamp.java
+++ src/nieszczescialolowe/test/TestRegExChamp.java
@@ -20,11 +20,33 @@ public class TestRegExChamp {
 	
 	@Test
 	public void averageStatsChamp() {
-		fail("Not yet implemented");
+		assertEquals(
+				new StringBuilder()
+					.append("Average KDACss - 10/6/15/295\n")
+					.append("Most played lane - Mid\n")
+					.append("Most played lane % - 100.0\n")
+					.append("Win ratio % - 81.82\n")
+					.append("Average game time - 0:46:29\n")
+					.append("Most often grade - S\n")
+					.append("Most often grade % - 54.55\n")
+					.append("Average afk's - 0.0\n")
+					.toString(),
+				Tests.runCommand("average stats champ Fizz"));
 	}
 	
 	@Test
 	public void averageStatsChampX() {
-		fail("Not yet implemented");
+		assertEquals(
+				new StringBuilder()
+					.append("Average KDACss - 6/6/9/190\n")
+					.append("Most played lane - Top\n")
+					.append("Most played lane % - 66.67\n")
+					.append("Win ratio % - 66.67\n")
+					.append("Average game time - 0:37:05\n")
+					.append("Most often grade - A\n")
+					.append("Most often grade % - 33.33\n")
+					.append("Average afk's - 2.0\n")
+					.toString(),
+				Tests.runCommand("average stats champ Pantheon 3"));
 	}
 }
